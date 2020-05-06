f21f6a02d359442ea045df672d779c75f6c9c3c9
ashakirzianov <ashakirzianov@icloud.com>
2020-05-05 21:32:11 -0300

    Featured

---
+++ atoms/Featured.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BooqCardProps, BooqCard } from './BooqCard';
+
+export function Featured({ cards }: {
+    cards: BooqCardProps[],
+}) {
+    return <div>
+        {
+            cards.map(
+                card => <BooqCard {...card} />
+            )
+        }
+    </div>;
+}
