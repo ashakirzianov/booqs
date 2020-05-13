3c9c38218e85507cb8adb940377f6cc502341dff
ashakirzianov <ashakirzianov@icloud.com>
2020-05-13 15:48:03 -0300

    Setup apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,3 @@
-import { AppProps } from 'next/app'
-
 // Tippy styles
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/dist/svg-arrow.css';
@@ -7,10 +5,23 @@ import 'tippy.js/dist/border.css';
 import 'tippy.js/animations/shift-away.css';
 
 // FontAwesome support
-import { config } from '@fortawesome/fontawesome-svg-core' // 👈
-import '@fortawesome/fontawesome-svg-core/styles.css' // 👈
-config.autoAddCss = false // 👈
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
+import { AppProps } from 'next/app';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import fetch from 'isomorphic-unfetch';
+import { appConfig } from '../lib';
+
+const client = new ApolloClient({
+    uri: appConfig().apiUri,
+    fetch: fetch,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return <ApolloProvider client={client}>
+        <Component {...pageProps} />
+    </ApolloProvider>;
 }
