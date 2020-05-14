f71c759b8345764ffdbb5d793790fb118427cad5
ashakirzianov <ashakirzianov@icloud.com>
2020-05-14 11:38:12 -0300

    Move 'BooqCard' to 'Featured'

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
-import { BooqCard, BooqCardProps } from '../controls/BooqCard';
+import { meter } from '../controls/meter';
+import { LinkButton } from '../controls/Buttons';
+import { Panel } from '../controls/Panel';
+import { BooqTags, BooqTag } from '../controls/BooqTags';
+import { BooqCover } from '../controls/BooqCover';
+import { boldWeight } from '../controls/theme';
 
+type BooqCard = {
+    title?: string,
+    author?: string,
+    cover?: string,
+    tags: BooqTag[],
+}
 type Featured = {
-    cards: BooqCardProps[],
+    cards: BooqCard[],
     loading: boolean,
 }
 function useFeatured(): Featured {
@@ -31,7 +42,7 @@ export function Featured() {
     return <div>
         {
             cards.map(
-                (card, idx) => <BooqCard key={idx} {...card} />
+                (card, idx) => <FeaturedCard key={idx} {...card} />
             )
         }
         <style jsx>{`
@@ -41,5 +52,86 @@ export function Featured() {
                 align-items: center;
             }
             `}</style>
+    </div>;
+}
+
+function FeaturedCard({
+    title, author, cover, tags,
+}: BooqCard) {
+    return <Panel>
+        <div className="container">
+            <BooqCover
+                title={title}
+                author={author}
+                cover={cover}
+            />
+            <div className="details">
+                <div>
+                    <Header title={title} author={author} />
+                    <BooqTags tags={tags} />
+                </div>
+                <Actions />
+            </div>
+            <style jsx>{`
+            .container {
+                display: flex;
+                flex-direction: row;
+                flex: 1;
+                padding: ${meter.large};
+            }
+            .details {
+                display: flex;
+                flex-direction: column;
+                flex: 1;
+                justify-content: space-between;
+                margin-left: ${meter.xxLarge};
+            }
+            `}</style>
+        </div>
+    </Panel>;
+}
+
+function Header({ title, author }: {
+    title?: string,
+    author?: string,
+}) {
+    return <div>
+        <span className="title">{title}</span>
+        <span className="author">by {author}</span>
+        <style jsx>{`
+            div {
+                display: flex;
+                flex-flow: column wrap;
+                align-items: baseline;
+            }
+            .title {
+                font-size: x-large;
+                font-weight: ${boldWeight};
+            }
+            .author {
+                font-size: large;
+            }
+            `}</style>
+    </div>;
+}
+
+function Actions() {
+    return <div>
+        <div>
+            <LinkButton text="Add +" />
+        </div>
+        <div>
+            <LinkButton text="Read &rarr;" />
+        </div>
+        <style jsx>{`
+        div {
+            display: flex;
+            align-self: stretch;
+            justify-content: flex-end;
+        }
+        div > * {
+            margin: 0 0 0 ${meter.xLarge};
+        }
+        `}</style>
     </div>;
 }
