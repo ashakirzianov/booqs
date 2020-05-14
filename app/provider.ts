c14b4991a84c1834420592b473549e5c7ad6e2ef
ashakirzianov <ashakirzianov@icloud.com>
2020-05-14 17:32:00 -0300

    Move AppProvider to it's own file

---
+++ app/provider.ts
@@ -0,0 +1,29 @@
+import { ReactNode, createElement } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import fetch from 'isomorphic-unfetch';
+import { initialPaletteData } from './palette';
+
+
+const client = new ApolloClient({
+    uri: process.env.NEXT_PUBLIC_BACKEND,
+    fetch: fetch,
+    resolvers: [],
+});
+client.cache.writeData({
+    data: {
+        ...initialPaletteData,
+    },
+});
+
+export function AppProvider({ children }: {
+    children: ReactNode,
+}) {
+    return createElement(
+        ApolloProvider,
+        {
+            client,
+            children,
+        },
+    );
+}
