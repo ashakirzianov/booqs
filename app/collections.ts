7c8a32870260c3db0bdb3020dd47146bed11a0d6
ashakirzianov <ashakirzianov@icloud.com>
2020-05-18 18:20:57 -0300

    Collections hooks

---
+++ app/collections.ts
@@ -0,0 +1,53 @@
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const CollectionQuery = gql`query Collection($name: String!) {
+    collection(name: $name) {
+        booqs {
+            id
+            title
+            author
+            cover(size: 120)
+        }
+    }
+}`;
+type CollectionData = {
+    collection: {
+        booqs: {
+            id: string,
+            title?: string,
+            author?: string,
+            cover?: string,
+        }[],
+    },
+};
+
+export function useCollection(name: string) {
+    const { loading, data } = useQuery<CollectionData>(
+        CollectionQuery,
+        { variables: { name } }
+    );
+    return {
+        loading,
+        booqs: data?.collection?.booqs ?? [],
+    };
+}
+
+const AddToCollectionMutation = gql`
+mutation AddToCollection($name: String!, $booqId: ID!) {
+    addToCollection(name: $name, booqId: $booqId)
+}`;
+type AddToCollectionData = {
+    addToCollection: boolean,
+};
+export function useAddToCollection() {
+    const [doAdd, { loading }] = useMutation<AddToCollectionData>(
+        AddToCollectionMutation,
+    );
+    return {
+        loading,
+        addToCollection(booqId: string, name: string) {
+            doAdd({ variables: { name, booqId } });
+        },
+    };
+}
