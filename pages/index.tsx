2a473ade236fe4ca6bdcf8ac41c4873c4455d4c1
ashakirzianov <ashakirzianov@icloud.com>
2020-05-18 18:46:25 -0300

    Show reading list

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,16 @@ import { Page } from "../components/Page";
 import { AppBar } from "../components/AppBar";
 import { Featured } from "../components/Featured";
 import { ReadingHistory } from "../components/ReadingHistory";
+import { Collection } from "../components/Collection";
 
 export default function Home() {
   return <Page title="Booqs">
     <AppBar />
     <ReadingHistory />
+    <Collection
+      name='reading-list'
+      title='Reading List'
+    />
     <Featured />
     <style jsx>{`
       div {
