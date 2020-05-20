aef36fd35a24acbe388bbd3bb54c0b2e04ff876f
ashakirzianov <ashakirzianov@icloud.com>
2020-05-20 16:36:57 -0300

    Fix Search input positioning

--- components/Search.tsx
+++ components/Search.tsx
@@ -64,13 +64,12 @@ export function Search() {
             justify-content: flex-start;
             margin: 0;
             color: ${primary};
-            max-height: 2rem;
+            max-height: 3rem;
             overflow: visible;
         }
         .content {
             display: flex;
             position: relative;
-            top: -${meter.regular};
             flex-direction: column;
             background-color: ${background};
             border-radius: ${radius};
@@ -80,7 +79,7 @@ export function Search() {
             border: none;
             width: 100%;
             min-width: 0px;
-            margin: ${meter.regular} ${meter.regular} ${meter.regular} ${meter.large};
+            margin: ${meter.regular} ${meter.large};
             font: inherit;
             font-size: x-large;
             font-weight: ${normalWeight};
