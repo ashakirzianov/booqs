5f9207fd82aeea71817a66dc665cb7a60d529b2b
ashakirzianov <ashakirzianov@icloud.com>
2020-06-01 19:01:58 -0300

    Basic highlights hooks

---
+++ app/highlights.ts
@@ -0,0 +1,82 @@
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { BooqPath, uuid } from "core";
+
+const HighlightsQuery = gql`query HighlightsQuery($bookId: ID!) {
+    booq(id: $bookId) {
+        highlights {
+            id
+            start
+            end
+            group
+        }
+    }
+}`;
+type HighlightsData = {
+    booq: {
+        highlights: {
+            __typename: 'BooqHighlight',
+            id: string,
+            start: BooqPath,
+            end: BooqPath,
+            group: string,
+        }[],
+    },
+};
+export function useHighlights(booqId: string) {
+    const { loading, data } = useQuery<HighlightsData>(
+        HighlightsQuery,
+        { variables: { booqId } },
+    );
+    return {
+        loading,
+        highlights: (data?.booq.highlights ?? []),
+    };
+}
+
+const AddHighlightMutation = gql`mutation AddHighlight($highlight: HighlightInput) {
+    addHighlight(highlight: $highlight)
+}`;
+type AddHighlightData = { addHighlight: boolean };
+type AddHighlightVars = {
+    highlight: {
+        id: string,
+        booqId: string,
+        group: string,
+        start: BooqPath,
+        end: BooqPath,
+    },
+};
+export function useHighlightMutations(booqId: string) {
+    const [add] = useMutation<AddHighlightData, AddHighlightVars>(
+        AddHighlightMutation,
+    );
+    return {
+        addHighlight(input: {
+            start: BooqPath,
+            end: BooqPath,
+            group: string,
+        }) {
+            const id = uuid();
+            add({
+                variables: { highlight: { ...input, booqId, id } },
+                optimisticResponse: { addHighlight: true },
+                update(cache, { data }) {
+                    if (data?.addHighlight) {
+                        const cached = cache.readQuery<HighlightsData>({
+                            query: HighlightsQuery,
+                            variables: { booqId },
+                        });
+                        if (cached) {
+                            cached.booq.highlights.push({
+                                ...input,
+                                __typename: 'BooqHighlight',
+                                id,
+                            });
+                        }
+                    }
+                },
+            });
+        },
+    };
+}
