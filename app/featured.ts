66800dce09fefb77458ef7417b1c7f79e4c94ce2
ashakirzianov <ashakirzianov@icloud.com>
2020-06-03 21:01:03 -0300

    Patch

--- app/featured.ts
+++ app/featured.ts
@@ -39,5 +39,6 @@ export async function fetchFeatured() {
     const result = await doQuery<FeaturedData>({
         query: FeaturedQuery,
     });
-    return result.data?.featured ?? [];
+    return (result.data?.featured ?? [])
+        .filter(f => f !== null && f !== undefined);
 }
