797f98655b1e9c36106cbe09199d75b87fcd092c
ashakirzianov <ashakirzianov@icloud.com>
2020-06-08 16:48:42 -0300

    Send source on booq upload

--- app/upload.ts
+++ app/upload.ts
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 import { useMutation } from "@apollo/react-hooks";
+import { currentSource } from './common';
 
 export type FileData = any;
 
@@ -15,18 +16,26 @@ type UploadEpubData = {
         title?: string,
     },
 };
+type UploadEpubVariables = {
+    file: FileData,
+    source: string,
+};
 
 export type UploadState = ReturnType<typeof useUpload>;
 export function useUpload() {
     const [
         uploadEpub,
         { loading, data: uploadData },
-    ] = useMutation<UploadEpubData>(UploadEpubMutation);
+    ] = useMutation<UploadEpubData, UploadEpubVariables>(
+        UploadEpubMutation,
+    );
     return {
         uploading: loading,
         uploaded: uploadData?.uploadEpub,
         upload(file: FileData) {
-            uploadEpub({ variables: { file } });
+            uploadEpub({
+                variables: { file, source: currentSource() },
+            });
         },
     };
 }
