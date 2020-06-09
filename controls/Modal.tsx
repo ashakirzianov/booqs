03500ca3dfde714fffca9cdeb2474137d7e47490
ashakirzianov <ashakirzianov@icloud.com>
2020-06-08 22:17:02 -0300

    Change mobal background

--- controls/Modal.tsx
+++ controls/Modal.tsx
@@ -63,7 +63,7 @@ function ModalContent({
                 top: 0; right: 0; bottom: 0; left: 0;
                 justify-content: center;
                 align-items: center;
-                background: rgba(0, 0, 0, 0.5);
+                background: rgba(0, 0, 0, 0.25);
                 z-index: 10;
             }
             .container {
