328b9b4064f94e4a304bc1708eb489e00817a656
ashakirzianov <ashakirzianov@icloud.com>
2020-06-10 20:06:20 -0300

    Mobile layout

--- components/BooqLayout.tsx
+++ components/BooqLayout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { meter, headerHeight } from 'controls/theme';
+import { meter, headerHeight, vars } from 'controls/theme';
 
 type ControlsProps = {
     isVisible: boolean,
@@ -57,62 +57,85 @@ function BooqControls({
     CurrentPage, PagesLeft,
 }: ControlsProps) {
     return <div className='container'>
-        <div className='controls-a'>
-            <div className='main'>{MainButton}</div>
-            <div className='navigation'>{NavigationButton}</div>
-        </div>
-        <div className='controls-b'>
-            <div className='themer'>{ThemerButton}</div>
-            <div className='account'>{AccountButton}</div>
-        </div>
-        <div className='current-page'>
-            {CurrentPage}
-        </div>
-        <div className='pages-left'>
-            {PagesLeft}
-        </div>
+        <div className='main'>{MainButton}</div>
+        <div className='nav'>{NavigationButton}</div>
+        <div className='themer'>{ThemerButton}</div>
+        <div className='account'>{AccountButton}</div>
+        <div className='page'>{CurrentPage}</div>
+        <div className='left'>{PagesLeft}</div>
+        <div className='content' />
+        <div className='filler-top' />
+        <div className='filler-bottom' />
         <style jsx>{`
             .container {
                 position: fixed;
                 top: 0; bottom: 0; left: 0; right: 0;
                 pointer-events: none;
                 display: grid;
-                grid-template-columns: 1fr ${contentWidth} 1fr;
+                justify-items: center;
+                align-items: center;
+                grid-template-columns: auto auto 1fr ${contentWidth} 1fr auto auto;
                 grid-template-rows: ${headerHeight} 1fr ${headerHeight};
                 grid-template-areas: 
-                    "left-top . right-top"
-                    "left-middle . right-middle"
-                    "left-bottom . right-bottom";
+                    "main nav . content . themer account"
+                    ".    .   . content . .      .      "
+                    "page .   . content . .      left   ";
             }
-            .controls-a, .controls-b, .current-page, .pages-left {
+            .content {
+                grid-area: content;
+                align-self: stretch;
+                justify-self: stretch;
+            }
+            .main, .nav, .themer, .account {
                 pointer-events: auto;
+                padding: 0 ${meter.large};
             }
-            .main, .navigation, .themer, .account {
-                margin: 0 ${meter.large};
+            .main {
+                grid-area: main;
             }
-            .controls-a {
-                grid-area: left-top;
-                display: flex;
-                justify-content: flex-start;
-                align-items: center;
+            .nav {
+                grid-area: nav;
             }
-            .controls-b {
-                grid-area: right-top;
-                display: flex;
-                justify-content: flex-end;
-                align-items: center;
+            .themer {
+                grid-area: themer;
             }
-            .current-page {
-                grid-area: left-bottom;
-                display: flex;
-                justify-content: flex-start;
-                align-items: center;
+            .account {
+                grid-area: account;
             }
-            .pages-left {
-                grid-area: right-bottom;
-                display: flex;
-                justify-content: flex-end;
-                align-items: center;
+            .page {
+                grid-area: page;
+            }
+            .left {
+                grid-area: left;
+            }
+            .filler-top, .filler-bottom {
+                display: none;
+            }
+            @media (max-width: ${smallScreenWidth}) {
+                .container {
+                    grid-template-columns: auto auto 1fr auto auto;
+                    grid-template-rows: ${headerHeight} 1fr ${headerHeight};
+                    grid-template-areas: 
+                        "main nav content themer account"
+                        ".    .   content .      ."
+                        "page .   content .      left";
+                }
+                .filler-top, .filler-bottom {
+                    display: block;
+                    z-index: -1;
+                    grid-area: 1 / 1 / 1 / 6;
+                    align-self: stretch;
+                    justify-self: stretch;
+                    background: var(${vars.background});
+                }
+                .filler-top {
+                    grid-area: 1 / 1 / 1 / 6;
+                    box-shadow: 0px +2px 2px rgba(0, 0, 0, 0.1);
+                }
+                .filler-bottom {
+                    grid-area: 3 / 1 / 3 / 6;
+                    box-shadow: 0px -2px 2px rgba(0, 0, 0, 0.1);
+                }
             }
             `}</style>
     </div>;
