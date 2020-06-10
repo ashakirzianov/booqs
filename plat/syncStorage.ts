e9bfcbb656c0cc18dac5298ac4d8b0b1ce87d714
ashakirzianov <ashakirzianov@icloud.com>
2020-06-09 22:12:23 -0300

    Persist user settings

--- plat/syncStorage.ts
+++ plat/syncStorage.ts
@@ -2,18 +2,28 @@ export function syncStorageCell<T>(key: string) {
     const storage = process.browser
         ? window.localStorage
         : undefined;
+    function store(value: T) {
+        storage?.setItem(key, JSON.stringify(value));
+    }
+    function update(value: Partial<T>) {
+        const prev = restore();
+        if (prev) {
+            store({
+                ...prev,
+                ...value,
+            });
+        }
+    }
+    function restore(): T | undefined {
+        const value = storage?.getItem(key);
+        const parsed = value
+            ? JSON.parse(value) : undefined;
+        return parsed;
+    }
+    function clear() {
+        storage?.removeItem(key);
+    }
     return {
-        store(value: T) {
-            storage?.setItem(key, JSON.stringify(value));
-        },
-        restore(): T | undefined {
-            const value = storage?.getItem(key);
-            const parsed = value
-                ? JSON.parse(value) : undefined;
-            return parsed;
-        },
-        clear() {
-            storage?.removeItem(key);
-        },
+        store, update, restore, clear,
     };
 }
