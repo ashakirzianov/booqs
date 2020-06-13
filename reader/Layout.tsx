3039bde43a0c4eed5901b8ac6f455811e645d9e5
ashakirzianov <ashakirzianov@icloud.com>
2020-06-13 01:40:11 -0300

    Fix booq layout on safari

--- reader/Layout.tsx
+++ reader/Layout.tsx
@@ -73,6 +73,8 @@ function BooqControls({
             .container {
                 position: fixed;
                 top: 0; bottom: 0; left: 0; right: 0;
+                height: 100vh;
+                width: 100vw;
                 pointer-events: none;
                 justify-items: center;
                 align-items: center;
