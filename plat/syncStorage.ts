36f0d39f816b916f12526bc3a84c332d0e58bb2d
ashakirzianov <ashakirzianov@icloud.com>
2020-06-25 11:20:36 -0300

    Storage versioning

--- plat/syncStorage.ts
+++ plat/syncStorage.ts
@@ -1,9 +1,31 @@
-export function syncStorageCell<T>(key: string) {
+type CellValue<T> = {
+    value: T,
+    version: string | undefined,
+};
+export function syncStorageCell<T>(key: string, version?: string) {
     const storage = process.browser
         ? window.localStorage
         : undefined;
     function store(value: T) {
-        storage?.setItem(key, JSON.stringify(value));
+        const cellValue = {
+            value, version,
+        };
+        storage?.setItem(key, JSON.stringify(cellValue));
+    }
+    function restore(): T | undefined {
+        try {
+            const value = storage?.getItem(key);
+            const parsed = value
+                ? JSON.parse(value) as CellValue<T>
+                : undefined;
+            if (version === parsed?.version) {
+                return parsed?.value;
+            } else {
+                return undefined;
+            }
+        } catch {
+            return undefined;
+        }
     }
     function update(value: Partial<T>) {
         const prev = restore();
@@ -13,12 +35,6 @@ export function syncStorageCell<T>(key: string) {
                 ...value,
             });
         }
-    }
-    function restore(): T | undefined {
-        const value = storage?.getItem(key);
-        const parsed = value
-            ? JSON.parse(value) : undefined;
-        return parsed;
     }
     function clear() {
         storage?.removeItem(key);
