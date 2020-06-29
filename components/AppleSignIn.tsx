9830c3483651dc05ab925fb39b7856df12b26985
ashakirzianov <ashakirzianov@icloud.com>
2020-06-29 17:35:48 -0300

    Rethink 'sign in with Apple' button

--- components/AppleSignIn.tsx
+++ components/AppleSignIn.tsx
@@ -1,60 +1,47 @@
+/*global globalThis*/
 import Head from "next/head";
-import { useCustomDocumentEvent } from "controls/utils";
-import { ReactNode } from "react";
+import { useEffect } from "react";
+import { BorderButton } from "controls/Buttons";
+
+export function AppleSignInButton() {
+    return <BorderButton
+        icon='apple'
+        text='Apple'
+        onClick={() => {
+            appleSdk()?.auth.signIn().then(result => {
+                console.log('Apple sign in result');
+                console.log(result);
+            });
+        }}
+    />;
+}
 
 export function AppleSignInHead() {
+    useEffect(() => {
+        console.log(appleSdk());
+        appleSdk()?.auth.init({
+            clientId: process.env.NEXT_PUBLIC_APPLE_APP_ID,
+            scope: 'name email',
+            redirectURI: redirectUri(),
+            state: 'none',
+            usePopup: true,
+        });
+    }, []);
     return <Head>
-        <meta name="appleid-signin-client-id" content={process.env.NEXT_PUBLIC_APPLE_APP_ID} />
-        <meta name="appleid-signin-scope" content="name email" />
-        <meta name="appleid-signin-state" content="default" />
-        <meta name="appleid-signin-redirect-uri" content={redirectUrl()} />
-        <meta name="appleid-signin-use-popup" content="true" />
+        <script type="text/javascript" src="https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js" />
     </Head>;
 }
 
-function redirectUrl() {
+function appleSdk() {
+    return process.browser
+        ? globalThis.AppleID
+        : undefined;
+}
+
+function redirectUri() {
     const { protocol, host, pathname } = process.browser
         ? window.location
         : undefined ?? {};
     const url = `${protocol}//${host}${pathname}`;
     return url;
-}
-
-export function AppleSignInButton() {
-    return <AppleSignInButtonContainer>
-        <div className='button'>
-            Apple
-        </div>
-        <style jsx>{`
-            .button {
-                display: flex;
-                justify-content: center;
-                cursor: pointer;
-                width: 100px;
-                height: 50px;
-            }
-                `}</style>
-    </AppleSignInButtonContainer>;
-}
-
-function AppleSignInButtonContainer({ children }: {
-    children: ReactNode,
-}) {
-    useCustomDocumentEvent<any>('AppleIDSignInOnSuccess', data => {
-        console.log('success');
-        console.log(data);
-    });
-    useCustomDocumentEvent<any>('AppleIDSignInOnFailure', data => {
-        console.log('failure');
-        console.log(data);
-    });
-    return <>
-        <div id="appleid-signin" data-color="black" data-type="sign in">
-            {children}
-        </div>
-        <script
-            type="text/javascript"
-            src="https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js"
-        />
-    </>;
 }
