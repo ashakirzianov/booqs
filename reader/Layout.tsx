faafa811132b618e350b0b79b11b26ac049e3b5b
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-07-23 15:42:24 -0700

    Remerge BooqControls

--- reader/Layout.tsx
+++ reader/Layout.tsx
@@ -1,9 +1,21 @@
 import React, { ReactNode } from 'react'
+import styles from './ReaderLayout.module.css'
 import {
     meter, smallScreenWidth,
 } from '@/controls/theme'
-import { BooqControls, ControlsProps } from './BooqControls'
 
+type ControlsProps = {
+    isControlsVisible: boolean,
+    isNavigationOpen: boolean,
+    MainButton: ReactNode,
+    NavigationButton: ReactNode,
+    ThemerButton: ReactNode,
+    AccountButton: ReactNode,
+    CurrentPage: ReactNode,
+    PagesLeft: ReactNode,
+    NavigationContent: ReactNode,
+    ContextMenu: ReactNode,
+};
 
 type LayoutProps = ControlsProps & {
     BooqContent: ReactNode,
@@ -11,40 +23,42 @@ type LayoutProps = ControlsProps & {
     NextButton: ReactNode,
 };
 
-const contentWidth = '45rem'
 export function ReaderLayout({
     BooqContent, PrevButton, NextButton,
     ...controls
 }: LayoutProps) {
-    return <div className='layout'>
+    return <div className={`${styles.layout}`}>
         {PrevButton}
-        <div className='booq'>
+        <div className={`${styles.booq}`}>
             {BooqContent}
         </div>
         {NextButton}
         <BooqControls {...controls} />
-        <style jsx>{`
-            .layout {
-                display: flex;
-                flex: 1;
-                flex-flow: column;
-                align-items: center;
-            }
-            .booq {
-                display: flex;
-                flex-flow: column;
-                align-items: stretch;
-                width: 100%;
-                max-width: ${contentWidth};
-            }
-            @media (max-width: ${smallScreenWidth}) {
-                .booq {
-                    padding: 0 ${meter.large};
-                }
-                .layout {
-                    padding: ${meter.xxLarge} 0;
-                }
-            }
-            `}</style>
+    </div>
+}
+
+function BooqControls({
+    isControlsVisible, isNavigationOpen,
+    MainButton, NavigationButton,
+    ThemerButton, AccountButton,
+    CurrentPage, PagesLeft,
+    NavigationContent,
+    ContextMenu,
+}: ControlsProps) {
+    const showControls = isControlsVisible || isNavigationOpen
+    const showCtrlClass = showControls ? styles.showCtr : ''
+    const navOpenClass = isNavigationOpen ? styles.navopen : ''
+    return <div className={`${styles.reader}`}>
+        <div className={`${styles.main} ${showCtrlClass}`}>{MainButton}</div>
+        <div className={`${styles.nav} ${showCtrlClass}`}>{NavigationButton}</div>
+        <div className={`${styles.themer} ${showCtrlClass}`}>{ThemerButton}</div>
+        <div className={`${styles.account} ${showCtrlClass}`}>{AccountButton}</div>
+        <div className={`${styles.page} ${showCtrlClass}`}>{CurrentPage}</div>
+        <div className={`${styles.left} ${showCtrlClass}`}>{PagesLeft}</div>
+        <div className={`${styles.content} ${showCtrlClass}`} />
+        <div className={`${styles.backTop} ${showCtrlClass}`} />
+        <div className={`${styles.backBottom} ${showCtrlClass}`} />
+        <div className={`${styles.ctx} ${showCtrlClass}`}>{ContextMenu}</div>
+        <div className={`${styles.navc} ${navOpenClass}`}>{NavigationContent}</div>
     </div>
 }
