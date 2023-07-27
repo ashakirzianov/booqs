b0cd1117840741760237a0cfacfe427b87ad6f60
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-07-26 21:19:55 -0700

    Add error page

---
+++ app/error.tsx
@@ -0,0 +1,6 @@
+'use client'
+export default function BooqNotFound() {
+    return <div className='flex flex-col items-center justify-center h-screen w-screen font-main overflow-hidden text-2xl'>
+        <h1>App error</h1>
+    </div>
+}
