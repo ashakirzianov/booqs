adb50645a26b1949cdc03440d12333668e6792b8
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-08-05 22:48:22 -0700

    Fix schema loading

--- server/graphql/typeDefs.ts
+++ server/graphql/typeDefs.ts
@@ -1,8 +1,11 @@
 import { readFile } from 'fs'
+import path from 'path'
 
 export async function readTypeDefs() {
+    let root = process.cwd()
+    let schemaPath = path.join(root, 'server/graphql/schema.graphql')
     return new Promise<string>((resolve, reject) => {
-        readFile('./server/graphql/schema.graphql', {
+        readFile(schemaPath, {
             encoding: 'utf-8',
         }, (err, data) => {
             if (err) {
