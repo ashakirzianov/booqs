b7e429593c1314bb2fcff6b01bad3090a17e4c8e
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-08-14 21:36:21 -0700

    Remove 'Remove' button from 'Uploads' collection

--- app/(main)/account/collection/page.tsx
+++ app/(main)/account/collection/page.tsx
@@ -11,7 +11,6 @@ export default async function MyBooqs() {
         <BooqCollection
             title='Uploads'
             cards={uploads}
-            collection='uploads'
         />
         <BooqCollection
             title='My Booqs'
