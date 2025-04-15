ae1f0d8d4933369d36cb3a87c09293ee7582aa76
ashakirzianov <ashakirzianov@icloud.com>
2025-04-15 17:28:35 -0300

    Put back reading history

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,11 @@
 import { Featured } from '@/components/Featured'
+import ReadingHistory from '@/components/ReadingHistory'
 import { featuredBooqCards } from '@/data/booqs'
 
 export default async function Home() {
     const featured = await featuredBooqCards()
     return <>
+        <ReadingHistory />
         <Featured cards={featured} />
     </>
 }
