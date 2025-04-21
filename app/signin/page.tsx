1a3ba1d1549bc86882da42a5e1660262cef3f6e9
ashakirzianov <ashakirzianov@icloud.com>
2025-04-21 18:16:41 -0300

    returnTo search param

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,11 +1,17 @@
 import { Logo } from '@/components/Logo'
 import { SignInForm } from './SignInForm'
 
-export default async function Page() {
+export default async function Page({ searchParams }: {
+    searchParams: Promise<{
+        [key: string]: string | undefined
+    }>,
+}) {
+    let { returnTo } = await searchParams
+    returnTo = returnTo ?? '/'
     return <main className='flex flex-col items-center justify-start h-screen gap-4 p-16'>
         <Logo style={{
             fontSize: 'xxx-large',
         }} />
-        <SignInForm returnTo='/' />
+        <SignInForm returnTo={returnTo} />
     </main>
 }
