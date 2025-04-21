bc6dd2e848955f7b6c6322b4641caf30696d82ae
ashakirzianov <ashakirzianov@icloud.com>
2025-04-21 17:27:21 -0300

    Extract app logo component

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { ReactNode } from 'react'
 import { feedHref } from './Links'
+import { Logo } from './Logo'
 
 export function AppBar({ left, right }: {
     left?: ReactNode,
@@ -21,18 +22,4 @@ export function AppBar({ left, right }: {
             </div>
         </section>
     </nav>
-}
-
-function Logo() {
-    return <div className='font-normal' style={{
-        color: 'rgba(253,163,2,1)',
-        fontFamily: 'var(--font-main)',
-        fontSize: 'x-large',
-        background: '-webkit-linear-gradient(180deg, rgba(253,163,2,1) 50%, rgb(200, 145, 2) 100%)',
-        WebkitBackgroundClip: 'text',
-        WebkitTextFillColor: 'transparent',
-        userSelect: 'none',
-    }}>
-        BOOQS
-    </div>
 }
---
+++ components/Logo.tsx
@@ -0,0 +1,13 @@
+export function Logo() {
+    return <div className='font-normal' style={{
+        color: 'rgba(253,163,2,1)',
+        fontFamily: 'var(--font-main)',
+        fontSize: 'x-large',
+        background: '-webkit-linear-gradient(180deg, rgba(253,163,2,1) 50%, rgb(200, 145, 2) 100%)',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+        userSelect: 'none',
+    }}>
+        BOOQS
+    </div>
+}
