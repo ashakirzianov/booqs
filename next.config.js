5838168d8bee65b214cf525b6fd1164d37ca932c
ashakirzianov <ashakirzianov@icloud.com>
2025-05-07 10:45:33 -0300

    Setup bundle analyzer

--- next.config.js
+++ next.config.js
@@ -23,4 +23,8 @@ const nextConfig = {
     },
 }
 
-module.exports = nextConfig
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer(nextConfig)
