afb39af6fb472bac3fe4dfe20b2bd4fbd534f87a
ashakirzianov <ashakirzianov@icloud.com>
2025-05-12 23:58:10 -0300

    Use 'fflate' instead of 'jszip'

--- parser/zip.ts
+++ parser/zip.ts
@@ -1,23 +1,17 @@
 import { FileProvider } from 'booqs-epub'
-import JSZip from 'jszip'
+import { unzipSync, strFromU8 } from 'fflate'
 
-export type BinaryType = globalThis.Buffer<ArrayBufferLike>
+export type BinaryType = globalThis.Buffer
+
 export function createZipFileProvider(fileContent: Buffer): FileProvider<BinaryType> {
-    let _zip: Promise<JSZip> | undefined
-    async function zip() {
-        if (!_zip) {
-            _zip = JSZip.loadAsync(fileContent)
-        }
-        return _zip
-    }
+    const files = unzipSync(new Uint8Array(fileContent))
+
     return {
         async readText(path, diags) {
             try {
-                const file = (await zip()).file(path)
-                if (!file) {
-                    return undefined
-                }
-                return file.async('text')
+                const data = files[path]
+                if (!data) return undefined
+                return strFromU8(data)
             } catch (e) {
                 diags?.push({
                     message: `Error reading text ${path}: ${e}`,
@@ -27,11 +21,9 @@ export function createZipFileProvider(fileContent: Buffer): FileProvider<BinaryT
         },
         async readBinary(path, diags) {
             try {
-                const file = (await zip()).file(path)
-                if (!file) {
-                    return undefined
-                }
-                return file.async('nodebuffer')
+                const data = files[path]
+                if (!data) return undefined
+                return Buffer.from(data)
             } catch (e) {
                 diags?.push({
                     message: `Error reading binary ${path}: ${e}`,
