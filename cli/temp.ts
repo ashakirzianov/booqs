afa7375d40f01a311b4c652c41e9b0ea1164f9f0
ashakirzianov <ashakirzianov@icloud.com>
2025-07-31 15:03:39 -0300

    Temp cli command

--- cli/temp.ts
+++ cli/temp.ts
@@ -1,5 +1,18 @@
+// import { redis } from '@/backend/db'
 import { CliOptions } from './main'
 
 export async function temp(_options: CliOptions) {
-    console.info('Temporary command executed')
+    // let current = '0'
+    // const set = new Set<string>()
+    // let count = 0
+    // do {
+    //     const [cursor, keys] = await redis.scan(current, {
+    //         match: 'cache:*',
+    //         count: 1000,
+    //     })
+    //     await redis.del(...keys)
+    //     console.log(`Batch ${count++}: Found ${keys.length} keys, current cursor: ${cursor}`)
+    //     current = cursor
+    // } while (current !== '0')
+    // console.log(set)
 }
