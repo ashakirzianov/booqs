2041d23a05fb984b37c538c7a760ea26221e4277
ashakirzianov <ashakirzianov@icloud.com>
2025-07-31 16:58:12 -0300

    Cli: cleanup command

---
+++ cli/cleanup.ts
@@ -0,0 +1,37 @@
+import { redis } from '@/backend/db'
+import { CliOptions } from './main'
+
+export async function cleanup(options: CliOptions) {
+    const [command] = options.commands
+    switch (command) {
+        case 'cache':
+            await removeRedisKeys('cache:*')
+            return
+        case 'images':
+            await removeRedisKeys('images:*')
+            return
+        default:
+            console.info('Unknown cleanup command: ', command)
+            console.info('Available commands: cache, images')
+            return
+    }
+
+}
+
+async function removeRedisKeys(match: string) {
+    let current = '0'
+    const set = new Set<string>()
+    let count = 0
+    do {
+        const [cursor, keys] = await redis.scan(current, {
+            match,
+            count: 1000,
+        })
+        console.log(`Batch ${count++}: Found ${keys.length} keys, current cursor: ${cursor}`)
+        if (keys.length > 0) {
+            await redis.del(...keys)
+        }
+        current = cursor
+    } while (current !== '0')
+    console.log(set)
+}
