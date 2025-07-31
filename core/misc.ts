be64ef2a1350e0b21429c3850262860e852a75cd
ashakirzianov <ashakirzianov@icloud.com>
2025-07-31 17:38:03 -0300

    Fix 'parseId' utility

--- core/misc.ts
+++ core/misc.ts
@@ -1,7 +1,7 @@
 import { BooqId, InLibraryId, LibraryId } from './model'
 
 export function parseId(booqId: BooqId): [libraryId: LibraryId, id: InLibraryId] {
-    return booqId.split(':') as [LibraryId, InLibraryId]
+    return booqId.split('-') as [LibraryId, InLibraryId]
 }
 
 export function assertNever(x: never) {
