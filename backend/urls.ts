47b60a98433a01e2d32e5fb4f00b2574ea34553a
Anton Shakirzianov <ashakirzianov@icloud.com>
2026-03-06 19:01:08 -0300

    Generate correct image url

--- backend/urls.ts
+++ backend/urls.ts
@@ -4,6 +4,6 @@ export const coverSizes = [60, 120, 210] as const
 export type CoverSize = typeof coverSizes[number]
 export function urlForBooqImageId(booqId: BooqId, imageId: string, width?: number): string {
     const assetId = `${booqId}/${imageId}`
-    const url = `${process.env.NEXT_PUBLIC_URL}/api/images/@${assetId}${width ? `w${width}` : ''}.webp`
+    const url = `${process.env.NEXT_PUBLIC_URL}/api/images/${assetId}@${width ? `w${width}` : ''}.webp`
     return url
 }
