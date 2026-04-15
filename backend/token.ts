d16ac77347ad7aa1beba50f04cea0d1f689cb262
Anton Shakirzianov <ashakirzianov@icloud.com>
2026-04-15 18:42:42 -0300

    Fix access token TTL (make 15 minutes instead of 15 seconds, that was a typo)

--- backend/token.ts
+++ backend/token.ts
@@ -4,7 +4,7 @@ import { config } from './config'
 import { redis } from './db'
 
 const issuer = 'booqs'
-export const ACCESS_TOKEN_TTL = 15 * 1 // 15 minutes in seconds
+export const ACCESS_TOKEN_TTL = 15 * 60 // 15 minutes in seconds
 export const REFRESH_TOKEN_TTL = 60 * 60 * 24 * 30 // 30 days in seconds
 const REFRESH_TOKEN_PREFIX = 'refresh:'
 
