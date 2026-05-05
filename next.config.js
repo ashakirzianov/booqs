12f431d9dff676d358188b1ed18313c00543fdb1
Anton Shakirzianov <ashakirzianov@icloud.com>
2026-05-05 11:41:04 -0300

    Only set HSTS header in production

--- next.config.js
+++ next.config.js
@@ -9,14 +9,17 @@ const nextConfig = {
         },
     },
     async headers() {
+        const headers = [
+            { key: 'X-Content-Type-Options', value: 'nosniff' },
+            { key: 'X-Frame-Options', value: 'DENY' },
+            { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+        ]
+        if (process.env.NODE_ENV === 'production') {
+            headers.push({ key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' })
+        }
         return [{
             source: '/(.*)',
-            headers: [
-                { key: 'X-Content-Type-Options', value: 'nosniff' },
-                { key: 'X-Frame-Options', value: 'DENY' },
-                { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-                { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-            ],
+            headers,
         }]
     },
 }
